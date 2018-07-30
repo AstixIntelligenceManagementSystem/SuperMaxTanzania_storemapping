eac1ccccf0b83341b9c8a7240435fb1cdae6c8f8
SUNIL-PC <astixset2@gmail.com>
2018-07-30 14:33:20 +0530

    latest

---
+++ app/src/main/java/com/example/android/superMaxTanzania_storemapping/CommonInfo.java
@@ -0,0 +1,135 @@
+package com.example.android.superMaxTanzania_storemapping;
+
+
+import java.io.File;
+import android.net.Uri;
+public class CommonInfo
+{
+
+
+	// Its for Live  Path on 194 server for SFA
+
+
+
+
+/*
+	public static File imageF_savedInstance=null;
+	public static String imageName_savedInstance=null;
+	public static String clickedTagPhoto_savedInstance=null;
+	public static Uri uriSavedImage_savedInstance=null;
+
+	public static String imei="";
+
+	public static String fileContent="";
+	public static final String MejiDairyPreference="SuperMaxTanzaniaPreference";
+	public static String WebServicePath="http://103.20.212.194/WebServiceAndroidSuperMaxTanzaniaSFALive/Service.asmx";
+
+	public static int DistanceRange=3000;//Defautl Distance Range to show for store list
+
+
+	public static String VersionDownloadPath="http://103.20.212.194/downloads/";
+	public static String VersionDownloadAPKName="SuperMaxTanzaniaSM.apk";
+
+	public static String DATABASE_NAME = "DBAdapterSuperMaxTanzania";
+
+	public static int AnyVisit = 0;
+
+	public static int DATABASE_VERSIONID = 8;      // put this field value based on value in table on the server
+	public static String AppVersionID = "1.1";   // put this field value based on value in table on the server
+	public static int Application_TypeID = 1; //1=Parag Store Mapping,2=Parag SFA Indirect,3=Parag SFA Direct
+
+
+	public static String OrderSyncPath="http://103.20.212.194/ReadXML_SuperMaxTanzaniaSFALive/Default.aspx";
+	public static String ImageSyncPath="http://103.20.212.194/ReadXML_SuperMaxTanzaniaImagesLive/Default.aspx";
+
+	public static String RSPLOrderXMLFolder="SuperMaxTanzaniaSMXml";
+	public static String RSPLImagesnFolder="SuperMaxTanzaniaSMImages";
+	public static String FinalLatLngJsonFile="SuperMaxTanzaniaSMFinalLatLngJson";
+	public static String AppLatLngJsonFile="SuperMaxTanzaniaSMLatLngJson";
+
+*/
+
+
+	// Its for Test  Path on 194 server for SFA aa
+
+
+	public static File imageF_savedInstance=null;
+	public static String imageName_savedInstance=null;
+	public static String clickedTagPhoto_savedInstance=null;
+	public static Uri uriSavedImage_savedInstance=null;
+
+	public static String imei="";
+
+	public static String fileContent="";
+	public static final String MejiDairyPreference="SuperMaxTanzaniaPreference";
+	public static String WebServicePath="http://103.20.212.194/WebServiceAndroidSuperMaxTanzaniaSFATest/Service.asmx";
+
+	public static int DistanceRange=3000;//Defautl Distance Range to show for store list
+
+
+	public static String VersionDownloadPath="http://103.20.212.194/downloads/";
+	public static String VersionDownloadAPKName="SuperMaxTanzaniaSMTest.apk";
+
+	public static String DATABASE_NAME = "DBAdapterSuperMaxTanzania";
+
+	public static int AnyVisit = 0;
+
+	public static int DATABASE_VERSIONID = 8;      // put this field value based on value in table on the server
+	public static String AppVersionID = "1.1";   // put this field value based on value in table on the server
+	public static int Application_TypeID = 1; //1=Parag Store Mapping,2=Parag SFA Indirect,3=Parag SFA Direct
+
+
+	public static String OrderSyncPath="http://103.20.212.194/ReadXML_SuperMaxTanzaniaSFATest/Default.aspx";
+	public static String ImageSyncPath="http://103.20.212.194/ReadXML_SuperMaxTanzaniaImagesTest/Default.aspx";
+
+	public static String RSPLOrderXMLFolder="SuperMaxTanzaniaSMXml";
+	public static String RSPLImagesnFolder="SuperMaxTanzaniaSMImages";
+	public static String FinalLatLngJsonFile="SuperMaxTanzaniaSMFinalLatLngJson";
+	public static String AppLatLngJsonFile="SuperMaxTanzaniaSMLatLngJson";
+
+
+
+	// Its for Development  Path on 194 server for SFA
+
+
+
+   /* public static File imageF_savedInstance=null;
+    public static String imageName_savedInstance=null;
+    public static String clickedTagPhoto_savedInstance=null;
+    public static Uri uriSavedImage_savedInstance=null;
+
+    public static String imei="";
+
+    public static String fileContent="";
+    public static final String MejiDairyPreference="SuperMaxTanzaniaPreference";
+	public static String WebServicePath="http://103.20.212.194/WebServiceAndroidSuperMaxTanzaniaSFADevelopment/Service.asmx";
+
+	public static int DistanceRange=3000;//Defautl Distance Range to show for store list
+
+
+    public static String VersionDownloadPath="http://103.20.212.194/downloads/";
+	public static String VersionDownloadAPKName="SuperMaxTanzaniaSMDev.apk";
+
+	public static String DATABASE_NAME = "DBAdapterSuperMaxTanzania";
+
+	public static int AnyVisit = 0;
+
+	public static int DATABASE_VERSIONID = 8;      // put this field value based on value in table on the server
+	public static String AppVersionID = "1.1";   // put this field value based on value in table on the server
+	public static int Application_TypeID = 1; //1=Parag Store Mapping,2=Parag SFA Indirect,3=Parag SFA Direct
+
+
+	public static String OrderSyncPath="http://103.20.212.194/ReadXML_SuperMaxTanzaniaSFADevelopment/Default.aspx";
+	public static String ImageSyncPath="http://103.20.212.194/ReadXML_SuperMaxTanzaniaImagesDevelopment/Default.aspx";
+
+	public static String RSPLOrderXMLFolder="SuperMaxTanzaniaSMXml";
+	public static String RSPLImagesnFolder="SuperMaxTanzaniaSMImages";
+	public static String FinalLatLngJsonFile="SuperMaxTanzaniaSMFinalLatLngJson";
+    public static String AppLatLngJsonFile="SuperMaxTanzaniaSMLatLngJson";
+
+*/
+
+
+
+
+}
